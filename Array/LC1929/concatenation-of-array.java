5a919394d513fc81c6ccc97c7366db5708be97b5
Chan Hyun Yoo <chanhyuny0515@epopcon.com>
2023-08-22 18:33:52 +0900

    Time: 1 ms (91.45%), Space: 44.66 MB (16.79%)

---
+++ Array/LC1929/concatenation-of-array.java
@@ -0,0 +1,11 @@
+package Array.LC1929;
+
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int[] ans = new int[nums.length * 2];
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] = ans[i + nums.length] = nums[i];
+        }
+        return ans;
+    }
+}
