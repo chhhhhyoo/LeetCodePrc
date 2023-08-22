4dcc25e78f8042b93bf1652f5adfcb568786e8b3
Chan Hyun Yoo <chanhyuny0515@epopcon.com>
2023-08-22 18:25:14 +0900

    Time: 0 ms (100%), Space: 38.90 MB (93.19%)

---
+++ Math/LC1342/number-of-steps-to-reduce-a-number-to-zero.java
@@ -0,0 +1,17 @@
+package Math.LC1342;
+
+class Solution {
+    public int numberOfSteps(int num) {
+        int step = 0;
+        while (num != 0) {
+            if (num % 2 == 0) {
+                num = num / 2;
+                step += 1;
+            } else {
+                num -= 1;
+                step += 1;
+            }
+        }
+        return step;
+    }
+}
