2d53c03cbb00ac1f6ad166eacb442784f30edf41
Chan Hyun Yoo <123328228+chhhhhyoo@users.noreply.github.com>
2025-03-10 22:10:50 +0900

    Time: 177 ms (47.58%), Space: 39.7 MB (31.47%) - LeetHub

--- 0883-car-fleet/0883-car-fleet.py
+++ 0883-car-fleet/0883-car-fleet.py
@@ -1,10 +1,10 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        pos_speed = sorted([[x,y] for x, y in zip(position, speed)], key= lambda x : x[0], reverse= True)
+        pos_speed = sorted(zip(position, speed), key= lambda x : x[0], reverse= True)
         time_needed = [(target - x[0])/x[1] for x in pos_speed]
-        cnt = 0
-        while time_needed:
-            needed = time_needed[0]
-            time_needed = [x - needed for x in time_needed if x - needed > 0]
-            cnt += 1
-        return  cnt
+        stack = []
+        for t in time_needed:
+            if stack and stack[-1] >= t:
+                continue
+            stack.append(t)
+        return len(stack)
