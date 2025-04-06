e958e5edffafd6b14343035ed85b7c1c286dc3fb
Chan Hyun Yoo <123328228+chhhhhyoo@users.noreply.github.com>
2025-04-06 13:23:23 +0900

    Time: 955 ms (18.48%), Space: 18.1 MB (19.67%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,8 +1,10 @@
+from collections import Counter
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        n = len(s1)
-        for i in range(len(s2)-n+1):
-            partial_s2 = s2[i:i+n]
-            if sorted(s1) == sorted(partial_s2):
+        len1, len2 = len(s1), len(s2)
+        s1_counter = Counter(s1)
+        for i in range(len2-len1+1):
+            s2_window = Counter(s2[i:i+len1])
+            if s2_window == s1_counter:
                 return True
         return False
